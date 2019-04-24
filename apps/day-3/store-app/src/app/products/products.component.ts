5e7e49d1922acf2f00ae822a3a22532e5f838bdb
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-04-24 10:06:52 +0530

    commit apps at the start of day 3

---
+++ apps/day-3/store-app/src/app/products/products.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent {
+  products: Product[] = [
+    {
+      name: 'iPhone X',
+      description: 'From Apple',
+      price: 50000,
+      isAvailable: false
+    },
+    {
+      name: 'Samsung',
+      description: 'From Samsung',
+      price: 30000,
+      isAvailable: true
+    },
+    {
+      name: 'Mi',
+      description: 'From Xiomi',
+      price: 20000,
+      isAvailable: true
+    }
+  ];
+
+  onProductCreated(newProduct: Product) {
+    this.products.unshift(newProduct);
+  }
+}
